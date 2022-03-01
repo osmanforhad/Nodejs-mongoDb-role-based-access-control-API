a88fa913d5f8a474a7cc0618058cfc510af55ef4
osmanforhad <osmanforhad93@gmail.com>
2022-03-01 18:08:43 +0600

    write method for usr login functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const res = require("express/lib/response");
 
 async function hashPassword(password) {
   return await bcrypt.hash(password, 10);
@@ -10,6 +11,7 @@ async function validatePassword(plainPassword, hashedPassword) {
   return await bcrypt.compare(plainPassword, hashedPassword);
 }
 
+//__Method for SignUp__//
 exports.signup = async (request, response, next) => {
   try {
     const { email, password, role } = request.body;
@@ -31,6 +33,27 @@ exports.signup = async (request, response, next) => {
     await newUser.save();
     response.json({
       data: newUser,
+      accessToken,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//__Method for User Login__//
+exports.login = async (request, response, next) => {
+  try {
+    const { email, password } = request.body;
+    const user = await User.findOne({ email });
+    if (!user) return next(new Error("Email does not exist"));
+    const validPassword = await validatePassword(password, user.password);
+    if (!validPassword) return next(new Error("Password is not correct"));
+    const accessToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "Id",
+    });
+    await User.findByIdAndUpdate(user._id, { accessToken });
+    response.status(200).json({
+      data: { email: user.email, role: user.role },
       accessToken,
     });
   } catch (error) {
