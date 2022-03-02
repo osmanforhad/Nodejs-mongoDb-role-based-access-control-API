fdfcb32d54cbec18b19ae9e72122978d2a8ee7f9
osmanforhad <osmanforhad93@gmail.com>
2022-03-02 16:30:56 +0600

    route setup for the app

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -60,3 +60,55 @@ exports.login = async (request, response, next) => {
     next(error);
   }
 };
+
+//__Method for get all Users__//
+exports.getUsers = async (request, response, next) => {
+  const users = await User.find({});
+  response.status(200).json({
+    data: users,
+  });
+};
+
+//__Method for get single User Details__//
+exports.getUser = async (request, response, next) => {
+  try {
+    const userId = request.params.userId;
+    const user = await User.findById(userId);
+    if (!user) return next(new Error("User does not exist"));
+    response.status(200).json({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//__Method for Update User__//
+exports.updateUser = async (request, response, next) => {
+  try {
+    const update = request.body;
+    const userId = request.params.userId;
+    await User.findByIdAndUpdate(userId, update);
+    const user = await User.findById(userId);
+    response.status(200).json({
+      data = user,
+      message: "User has been updated"
+    });
+  } catch (error) {
+    next(error)
+  }
+};
+
+//__Method for Delete Single User__//
+exports.deleteUser = async(request, response, next) => {
+  try {
+    const userId = request.params.userId;
+    await User.findByIdAndDelete(userId);
+    response.status(200).json({
+      data:null,
+      message: "Usr has been Deleted"
+    })
+  } catch (error) {
+    next(error)
+  }
+}
