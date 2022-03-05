8166aa6e4c0bb7db8962ffd737481d2e892a2542
osmanforhad <osmanforhad93@gmail.com>
2022-03-05 13:31:49 +0600

    modify req res paramiter of every functions as i like

--- server/server.js
+++ server/server.js
@@ -21,21 +21,21 @@ mongoose
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(async (req, res, next) => {
-  if (req.headers["x-access-token"]) {
+app.use(async (request, response, next) => {
+  if (request.headers["x-access-token"]) {
     try {
-      const accessToken = req.headers["x-access-token"];
+      const accessToken = request.headers["x-access-token"];
       const { userId, exp } = await jwt.verify(
         accessToken,
         process.env.JWT_SECRET
       );
       // If token has expired
       if (exp < Date.now().valueOf() / 1000) {
-        return res.status(401).json({
+        return response.status(401).json({
           error: "JWT token has expired, please login to obtain a new one",
         });
       }
-      res.locals.loggedInUser = await User.findById(userId);
+      response.locals.loggedInUser = await User.findById(userId);
       next();
     } catch (error) {
       next(error);
